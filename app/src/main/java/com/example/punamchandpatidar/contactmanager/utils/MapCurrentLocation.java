065227767c91e99f9d84fec0cd7f2296bb06ce09
PUNAMCHAND <punam.chand@berylsystem.com>
2018-05-12 17:04:17 +0530

    Add google map and marker

---
+++ app/src/main/java/com/example/punamchandpatidar/contactmanager/utils/MapCurrentLocation.java
@@ -0,0 +1,52 @@
+package com.example.punamchandpatidar.contactmanager.utils;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * Created by abc on 5/12/2018.
+ */
+
+public class MapCurrentLocation implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
